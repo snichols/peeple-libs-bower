a7de08dfa30cb3a3602aa8936d5f1f10bddc6a11
Stephen Nichols <snichols@therealm.io>
2016-11-07 14:38:21 -0600

    Pushing the disconnect timer to 10s

--- PeepleDevice.js
+++ PeepleDevice.js
@@ -23,7 +23,7 @@ function checkForStaleDevice() {
 
 	var now = Date.now()
 	var timeSinceLastPing = now - lastSuccessfulPingTime	
-	deviceState.stale = timeSinceLastPing > 5000
+	deviceState.stale = timeSinceLastPing > 10000
 
 	if(wasStateStale != deviceState.stale) {
 		if(wasStateStale) {
